0da41af59618cd0ddadb13cb2ec331e6e54ffdba
emanisaikumar01 <emanisaikumar01@gmail.com>
2025-08-21 17:56:44 +0530

    Added Palindrome Number solution

---
+++ strings/palindrome_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        original = x
+        reversed_num = 0
+        while x > 0:
+            digit = x % 10
+            reversed_num = reversed_num * 10 + digit
+            x //= 10
+        return original == reversed_num
